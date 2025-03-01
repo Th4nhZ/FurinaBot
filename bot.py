740c65c76daa92d55f6178c72ca3a7c1e1639989
ThanhZ <ndt1710bt@gmail.com>
2025-03-02 01:08:38 +0700

    refactor

--- bot.py
+++ bot.py
@@ -1,15 +1,47 @@
 from __future__ import annotations
 
-import discord, os, platform, wavelink
+import logging
+import traceback
 from aiohttp import ClientSession
-from asqlite import Pool
-from discord import Intents, Activity, ActivityType, Embed, app_commands, utils
-from discord.ext.commands import Bot, when_mentioned_or, errors
+from platform import python_version
 from typing import List
 
-from settings import DEFAULT_PREFIX, ACTIVITY_NAME, DEBUG_WEBHOOK
 
-class Furina(Bot):
+import discord
+import wavelink
+from asqlite import Pool
+from discord import app_commands, utils, Activity, ActivityType, Embed, Intents
+from discord.ext.commands import errors, Bot, Context, when_mentioned_or
+
+
+from settings import DEFAULT_PREFIX, ACTIVITY_NAME, DEBUG_WEBHOOK, CHECKMARK
+
+
+class FurinaCtx(Context):
+    async def tick(self) -> None:
+        """Reacts checkmark to the command message"""
+        try:
+            await self.message.add_reaction(CHECKMARK)
+        except discord.HTTPException:
+            pass
+    
+    @property
+    def pool(self) -> Pool:
+        """Shortcut for `FurinaBot.pool`"""
+        return self.bot.pool
+    
+    @property
+    def cs(self) -> ClientSession:
+        """Shortcut for `FurinaBot.cs`"""
+        return self.bot.cs
+    
+    @property
+    def embed(self) -> Embed:
+        """Shortcut for FurinaBot.embed"""
+        return self.bot.embed
+    
+
+class FurinaBot(Bot):
     """
     Customized `commands.Bot` class
 
@@ -24,7 +56,7 @@ class Furina(Bot):
     -----------
     .. code-block:: python
         async with aiohttp.ClientSession() as client_session, asqlite.create_pool("config.db") as pool:
-            async with Furina(pool=pool, client_session=client_session) as bot:
+            async with FurinaBot(pool=pool, client_session=client_session) as bot:
                 await bot.start(TOKEN)
     """
     def __init__(self, *, pool: Pool, client_session: ClientSession) -> None:
@@ -41,6 +73,13 @@ class Furina(Bot):
         )
         self.pool = pool
         self.cs = client_session
+
+    @property
+    def embed(self):
+        return Embed().set_footer(text="Coded by ThanhZ")
+    
+    async def get_context(self, message: discord.Message, *, cls = FurinaCtx):
+        return await super().get_context(message, cls=cls)
 
     async def create_prefix_table(self) -> None:
         """Create a `custom_prefixes` table in the database"""
@@ -62,37 +101,37 @@ class Furina(Bot):
         return when_mentioned_or(prefix)(self, message)
 
     async def on_ready(self) -> None:
-        print(f"Logged in as {self.user.name}")
-        print(f"discord.py version {discord.__version__}")
-        print(f"Wavelink version {wavelink.__version__}")
-        print(f"Python version {platform.python_version()}")
+        logging.info(f"Logged in as {self.user.name}")
 
         try:
-            embed = Embed(color=self.user.accent_color).set_author(
-                name="BOT IS READY!",
-                icon_url=self.user.display_avatar.url
-            )
+            embed = self.embed.set_author(name="BOT IS READY!")
+            embed.color = self.user.accent_color
             embed.timestamp = utils.utcnow()
-            discord.SyncWebhook.from_url(DEBUG_WEBHOOK).send(embed=embed)
+            webhook = discord.Webhook.from_url(DEBUG_WEBHOOK, client=self)
+            await webhook.send(embed=embed, avatar_url=self.user.display_avatar.url, username=self.user.display_name)
         except ValueError:
-            print("Cannot get the Webhook url for on_ready events."
-                  "If you don't want to get a webhook message when the bot is ready, please ignore this")
+            logging.warning("Cannot get the Webhook url for on_ready events."
+                            "If you don't want to get a webhook message when the bot is ready, please ignore this")
 
     async def setup_hook(self) -> None:
+        logging.info(f"discord.py v{discord.__version__}")
+        logging.info(f"Wavelink v{wavelink.__version__}")
+        logging.info(f"Running Python {python_version()}")
         await self.create_prefix_table()
         await self.update_prefixes()
 
         # loads the extensions
-        for filename in os.listdir("./_extensions"):
-            if filename.endswith(".py"):
-                extension = filename[:-3]
-                try:
-                    await self.load_extension(f"_extensions.{extension}")
-                    print(f"Loaded extension: {extension}")
-                except errors.NoEntryPointError:
-                    print(f"Extension {extension} has no setup function so it cannot be loaded")
-                except Exception as e:
-                    print(f"An error occured when trying to load {extension}\n{e}")
+        from _extensions import EXTENSIONS
+        for extension in EXTENSIONS:
+            try:
+                await self.load_extension(f"{extension}")
+                logging.info(f"Loaded extension: {extension}")
+            except errors.NoEntryPointError:
+                logging.error(f"Extension {extension} has no setup function so it cannot be loaded")
+            except Exception as e:
+                traceback.print_exc()
+                logging.error(f"An error occured when trying to load {extension}\n{e}")
         await self.load_extension("jishaku")
-        print("Loaded Jishaku extension")
+        logging.info("Loaded Jishaku extension")
+
 
--- main.py
+++ main.py
@@ -1,15 +1,72 @@
 from __future__ import annotations
-import aiohttp, asqlite, asyncio, discord
-from keep_alive import keep_alive
 
-from bot import Furina
+import asyncio
+import os
+import logging
+
+import asqlite
+from aiohttp import ClientSession
+
+
+from bot import FurinaBot
 from settings import TOKEN
+       
 
-async def main():
-    discord.utils.setup_logging()
-    async with aiohttp.ClientSession() as client_session, asqlite.create_pool("config.db") as pool:
-        async with Furina(pool=pool, client_session=client_session) as bot:
-            keep_alive()
+class LogFormatter(logging.Formatter):
+    grey = '\x1b[38;21m'
+    blue = '\x1b[38;5;39m'
+    yellow = '\x1b[38;5;226m'
+    red = '\x1b[38;5;196m'
+    bold_red = '\x1b[31;1m'
+    reset = '\x1b[0m'
+
+    def __init__(self):
+        super().__init__()
+        self.FORMATS = {
+            logging.DEBUG:    self.grey     + "%(asctime)s | %(levelname)8s" + self.reset + " | %(message)s",
+            logging.INFO:     self.blue     + "%(asctime)s | %(levelname)8s" + self.reset + " | %(message)s",
+            logging.WARNING:  self.yellow   + "%(asctime)s | %(levelname)8s" + self.reset + " | %(message)s",
+            logging.ERROR:    self.red      + "%(asctime)s | %(levelname)8s" + self.reset + " | %(message)s",
+            logging.CRITICAL: self.bold_red + "%(asctime)s | %(levelname)8s" + self.reset + " | %(message)s"
+        }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
+def handle_setup_logging() -> None:
+    """Setup logging for the bot"""
+    from discord import utils
+    timestamp = utils.utcnow().strftime("%Y%m%d_%H%M%S")
+    log_filename = f"logs/furina_{timestamp}.log"
+
+    file_handler = logging.FileHandler(log_filename)
+    console_handler = logging.StreamHandler()
+    file_handler.setFormatter(LogFormatter())
+    console_handler.setFormatter(LogFormatter())
+
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.INFO)
+    root_logger.addHandler(file_handler)
+    root_logger.addHandler(console_handler)
+
+    # utils.setup_logging()
+    
+def delete_old_logs() -> None:
+    """Delete old logs"""
+    logs = sorted([file for file in os.listdir("logs") if file.startswith("furina_") and file.endswith(".log")])
+    if len(logs) > 2:
+        for log in logs[:-2]:
+            os.remove(f"logs/{log}")
+
+async def main() -> None:
+    os.makedirs("logs", exist_ok=True)
+    delete_old_logs()
+    handle_setup_logging()
+    async with ClientSession() as client_session, asqlite.create_pool("config.db") as pool:
+        async with FurinaBot(pool=pool, client_session=client_session) as bot:
             await bot.start(TOKEN)
 
 asyncio.run(main())
